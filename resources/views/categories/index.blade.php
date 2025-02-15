09c15df11548c5692ec5d2ccba4ec55c3a29f860
Khari Woods <khariwoods3@gmail.com>
2025-02-15 11:49:04 -0400

    converted list into grid

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -2,12 +2,26 @@
 
 @section('content')
 <div class="container">
-    <h2>Expense Categories</h2>
-    <a href="{{ route('categories.create') }}" class="btn btn-success">Add New Category</a>
-    <ul class="list-group mt-3">
+
+<div class="mt-4">
+<h2>Expense Categories</h2>
+</div>
+    
+
+    <div class="mt-4">
+      <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-3">
         @foreach($categories as $category)
-            <li class="list-group-item">{{ $category->name }}</li>
+
+        <li class="list-group-item p-5 bg-blue-500 rounded text-white">{{ $category->name }}</li>  
+           
         @endforeach
     </ul>
+    </div>
+
+
+    <div class="mt-4">
+        <a href="{{ route('categories.create') }}" class="bg-sky-500 text-white font-bold py-2 px-4 rounded hover:bg-sky-700">Add New Category</a>
+    </div>
+  
 </div>
 @endsection
