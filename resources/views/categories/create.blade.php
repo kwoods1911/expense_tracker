d89d1e4bfb390f5fa55e6b671c7d425851b031a7
Khari Woods <khariwoods3@gmail.com>
2025-02-15 12:46:42 -0400

    position centered

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,15 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container flex flex-col items-center justify-start mt-8">
     <h2>Add New Category</h2>
-    <form action="{{ route('categories.store') }}" method="POST">
+    <form action="{{ route('categories.store') }}" method="POST" class="w-full max-w-md mt-8">
         @csrf
         <div class="mb-3">
             <label for="name" class="form-label">Category Name</label>
-            <input type="text" name="name" class="form-control" required>
+            <input type="text" name="name" class="form-control ml-8" required>
         </div>
-        <button type="submit" class="btn btn-success">Add Category</button>
+        <div class="flex justify-between mt-8">
+            <button type="submit" class="btn btn-success">Add Category</button>
+            <a href="{{ route('categories.index') }}" class="btn btn-secondary">Cancel</a>
+        </div>
     </form>
 </div>
 @endsection
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -12,7 +12,7 @@
       <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-3 min-h-[200px] shadow-xl p-4">
         @foreach($categories as $category)
 
-        <li class="list-group-item p-5 bg-blue-500 rounded text-white shadow-xl">{{ $category->name }}</li>  
+        <li class="list-group-item p-5 bg-blue-500 rounded text-white shadow-xl max-h-36">{{ $category->name }}</li>  
            
         @endforeach
     </ul>
