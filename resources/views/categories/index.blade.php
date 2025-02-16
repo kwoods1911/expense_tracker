08074cfa2cc7c0db680c53dd94454d0787e7bfc3
Khari Woods <khariwoods3@gmail.com>
2025-02-16 15:30:16 -0400

    add expense page updated

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -9,11 +9,9 @@
     
 
     <div class="mt-4">
-      <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-3 min-h-[200px] shadow-xl p-4">
+      <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-3 min-h-[300px] shadow-xl p-4">
         @foreach($categories as $category)
-
-        <li class="list-group-item p-5 bg-blue-500 rounded text-white shadow-xl max-h-36 flex items-center justify-center">{{ $category->name }}</li>  
-           
+        <li class="list-group-item p-5 bg-blue-500 rounded text-white text-2xl shadow-xl max-h-[100px] flex items-center justify-center">{{ $category->name }}</li>  
         @endforeach
     </ul>
     </div>
