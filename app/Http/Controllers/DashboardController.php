a6944457c25c5321687261d3cb7003f16710d327
Khari <khariwoods3@gmail.com>
2025-03-14 11:49:46 -0300

    Income data displaying

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Expense;
 use App\Models\Budget;
+use App\Models\Income;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
@@ -20,11 +21,22 @@ class DashboardController extends Controller
         
         $totalSpent = Expense::where('user_id', $user->id)->sum('amount');
 
+        $totalIncome = Income::where('user_id', $user->id)->sum('amount');
+
+    
+        $netSavings = number_format($totalIncome - $totalSpent, 2);
+
         // Spending by category
         $spendingByCategory = Expense::where('user_id', $user->id)
             ->selectRaw('category, SUM(amount) as total_spent')
             ->groupBy('category')
             ->pluck('total_spent', 'category');
+
+            // Spending by category
+        $incomeByCategory = Income::where('user_id', $user->id)
+        ->selectRaw('name, SUM(amount) as total_income')
+        ->groupBy('name')
+        ->pluck('total_income', 'name');
 
         // Recent expenses
         $recentExpenses = Expense::where('user_id', $user->id)
@@ -32,6 +44,7 @@ class DashboardController extends Controller
             ->limit(10)
             ->get();
             
-        return view('dashboard', compact('totalBudget', 'totalSpent', 'spendingByCategory', 'recentExpenses'));
+        return view('dashboard', compact('totalBudget', 'totalSpent',
+        'spendingByCategory', 'recentExpenses', 'totalIncome', 'netSavings','incomeByCategory'));
     }
 }
