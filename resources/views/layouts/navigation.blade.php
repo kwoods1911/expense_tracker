c5c09579ee2b9e1dcb47b64320c4122d82a6d242
Khari <khariwoods3@gmail.com>
2025-03-19 01:13:47 -0300

    test

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -52,9 +52,7 @@
                     </x-slot>
 
                     <x-slot name="content">
-                        <!-- <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
-                        </x-dropdown-link> -->
+                      
 
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
@@ -116,9 +114,7 @@
             </div>
 
             <div class="mt-3 space-y-1">
-                <!-- <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
-                </x-responsive-nav-link> -->
+                
 
                 <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
