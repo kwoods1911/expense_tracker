7113d09757c8102a18e53a6e17320b08a5d1df28
Khari <khariwoods3@gmail.com>
2025-03-28 15:46:36 -0300

    Database seeder in place

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,11 +3,15 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Expense;
 use Illuminate\Database\Seeder;
 use Database\Seeders\CategorySeeder;
 use Database\Seeders\UserSeeder;
 use Database\Seeders\BudgetSeeder;
 use Database\Seeders\IncomeCategorySeeder;
+use Database\Seeders\IncomeSeeder;
+use Database\Seeders\ExpenseSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,8 +21,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(IncomeCategorySeeder::class);
+        $this->call(IncomeSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(UserSeeder::class);
         $this->call(BudgetSeeder::class);
+        $this->call(ExpenseSeeder::class);
     }
 }
