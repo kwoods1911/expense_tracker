a60f82360188dff5cd1f7ba97f1e890234e47df7
Khari <khariwoods3@gmail.com>
2025-03-28 21:54:15 -0300

    Updated description page

--- resources/views/project-description.blade.php
+++ resources/views/project-description.blade.php
@@ -6,40 +6,37 @@
 </div>
 
 <div>
-    <h3>El Cheapo - Expense Tracker</h3>
+    <h3 class="font-bold">El Cheapo - Expense Tracker</h3>
 </div>
+
+<p class="mt-2">El Cheapo is a simple expense tracker designed to help users manage their finances effectively. The application includes the following features:</p>
 
 <div class="">
     <div class="mt-4">
-        <p>Key Features</p>
-      <ul>
+        <p class="font-bold text-2xl">Key Features</p>
+        
+    <ul>
         <li>Income & Budget Management – Users can input their income and set a budget to track their spending.</li>
         <li>Expense Tracking – Users can log expenses with details like category, amount, and date.</li>  
-        <li>Receipt Uploads – Supports uploading, editing, and managing receipts in PDF or JPG format, stored securely in an AWS S3 bucket.</li>
+        <li>Receipt Uploads – Supports uploading, editing, and managing receipts in PDF or JPG format, stored securely in an <span class="font-bold">AWS S3 bucket.</span></li>
         <li>Basic CRUD Operations – Users can create, read, update, and delete income, budget, and expense entries.</li>
-        </ul>
+    </ul>
 
-<p>Tech Stack & Deployment:</p>
+    
+<p class="mt-4 font-bold text-2xl">Tech Stack & Deployment:</p>
 
 
 <ul>    
-<li>Backend: Built with Laravel</li>
-<li>Frontend: Tailwind and blade template engine. Chart.js for displaying the data on the dashboard</li>
-<li>Storage: AWS S3 for receipt uploads.</li>
-<li>Hosting: AWS EC2 instance (Free Tier) for MVP deployment.</li>
+    <li>Backend: Built with <span class="font-bold">Laravel</span></li>
+    <li>Frontend: Tailwind and blade template engine. <span class="font-bold">Chart.js</span> for displaying the data on the dashboard</li>
+    <li>Storage: <span class="font-bold">AWS S3</span> for receipt uploads.</li>
+    <li>Hosting: <span class="font-bold">AWS EC2 instance</span> for deployment.</li>
 </ul>
 
-
-
 <p>This project showcases my ability to build and deploy full-stack applications while integrating cloud services for scalable storage.</p>
+</div>
 
-
-
-       
-
-    </div>
-
-    <p>More details will be emailed soon.</p>
+    <p>More details will be added soon.</p>
 </div>
 
 
