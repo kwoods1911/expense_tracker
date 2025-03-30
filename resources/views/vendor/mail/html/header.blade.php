26455bdf695dff666216b0e5ef1b2bfe06da46a5
Khari <khariwoods3@gmail.com>
2025-03-30 16:19:25 -0300

    Removed files and updated email template

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,9 +1,11 @@
 @props(['url'])
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
+<a href="{{ $url }}" style="display: inline-block; text-decoration: underline;">
 @if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+<!-- <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="El Cheapo"> -->
+
+<h1>El Cheapo</h1>
 @else
 {{ $slot }}
 @endif
