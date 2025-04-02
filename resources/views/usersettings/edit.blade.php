68449d70fce170f29a8ead9fa74857b5a8ad3f7f
Khari <khariwoods3@gmail.com>
2025-04-02 15:14:36 -0300

    User settings styling done

--- resources/views/usersettings/edit.blade.php
+++ resources/views/usersettings/edit.blade.php
@@ -3,14 +3,14 @@
 @section('content')
 
 
-<div>
+<div class="flex flex-col items-center justify-start mt-8">
 
-<h1>Notification Settings</h1>
+<h2 class="text-2xl">Notification Settings</h2>
 
 <form action="{{ route('update.setting', $user->id) }}" method="POST">
     @csrf
     @method('PUT')
-    <label for="receive_notifications">Receive Emails ?</label>
+    <label for="receive_notifications" class="form-label">Receive Emails ?</label>
     <input type="checkbox" name="receive_notifications" id="receiveEmails" value="1">
     <br>
 
@@ -18,13 +18,13 @@
 <label for="notification_time">Set Time to receive Emails</label>
 <span>Notifcations are sent once daily</span>
     <br>
-   <input type="time" name="notification_time" id="notification_time" value="{{ $user->formatted_notification_time }}">
+   <input class="ml-8 border border-gray-300 p-2 rounded focus:outline-none focus:ring-2 focus:ring-sky-500 mb-4 w-full" type="time" name="notification_time" id="notification_time" value="{{ $user->formatted_notification_time }}">
     <br>
     <span>Note: You will receive notifications only if you have selected the option to receive emails.</span>
 
     
     <label for="timezone">Select Your Timezone: </label>
-    <select name="timezone" id="timezone" required>
+    <select name="timezone" id="timezone" class="ml-8 border border-gray-300 p-2 rounded focus:outline-none focus:ring-2 focus:ring-sky-500 mb-4 w-full" required>
         @foreach(timezone_identifiers_list() as $timezone)
         <option value="{{ $timezone }}"  {{ $user->timezone === $timezone ? 'selected' : '' }}>
             {{ $timezone }}
@@ -33,7 +33,10 @@
     </select>
 
    <br>
-    <input type="submit" value="Save Settings">
+   <div class="flex justify-between mt-8">
+        <input class="bg-sky-500 text-white font-bold py-2 px-4 rounded hover:bg-sky-700" type="submit" value="Save Settings">
+        <a href="{{ route('usersettings') }}" class="bg-red-500 text-white font-bold py-2 px-4 rounded hover:bg-red-700">Cancel</a>
+    </div>
 </form>
 </div>
 
--- resources/views/usersettings/index.blade.php
+++ resources/views/usersettings/index.blade.php
@@ -4,22 +4,45 @@
 
 
 <div>
-
-<h1>User Settings</h1>
-<span>User name</span>
-<br>
-<span>notification time:  {{$user->formatted_notification_time }}</span>
-<br>
-
-<span>Receive Notifications: {{ $user->formatted_notification_selection }}</span>
-
-
-<br>
-
-<span>User Timezone: {{$user->formatted_user_timezone}}</span>
-<br>
-<a href="/editsettings/{{$user->id}}">Edit Settings</a>
-
+<h1 class="m-8 mt-4 text-4xl font-bold">User Settings</h1>
 </div>
 
+
+
+    
+<div class="block md:w-1/2 lg:w-1/3 mt-8">
+    <table class="table-auto border-collapse border border-gray-400 table w-full m-8 bg-white-700 p-8 mx-auto max-w-7xl shadow-xl">
+       
+       <tr class="table-row border p-8">
+           
+          <td class="py-2 px-2 font-bold">User Name</td>
+           <td>{{ $user->name }}</td>
+          </tr>
+
+          <tr class="table-row border p-8">
+           <td class="py-2 px-2 font-bold">Notification time:</td>
+           <td>{{$user->formatted_notification_time }}</td>
+          </tr>
+
+          <tr class="table-row border p-8">
+           <td class="py-2 px-2 font-bold">Receive Notifications: </td>
+           <td> {{ $user->formatted_notification_selection }}</td>
+          </tr>
+
+
+          <tr class="table-row border p-8">
+                <td class="py-2 px-2 font-bold">User Timezone: </td>
+                <td>
+                <span>{{$user->formatted_user_timezone}}</span>
+                </td>
+          </tr>
+
+          <tr class="table-row border p-8">
+            <td class="py-2 px-2 font-bold">
+                
+                <a class="mt-8 bg-sky-500 text-white font-bold py-2 px-4 rounded hover:bg-sky-700" href="/editsettings/{{$user->id}}">Edit</a>
+            </td>
+          </tr>
+       </table>
+    </div>
 @endsection
