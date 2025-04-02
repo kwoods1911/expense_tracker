d3dea4deea7f0ac4563fb604f4e1a369b0e40035
Khari <khariwoods3@gmail.com>
2025-04-02 01:03:43 -0300

    Notification seems to be working

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,8 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Models\Budget;
+use App\Models\User;
+
 
 
 class Kernel extends ConsoleKernel
@@ -12,17 +14,26 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      */
+ 
     protected function schedule(Schedule $schedule): void
     {
-
-      //If current user has budgets notification enabled
-       $schedule->call(function(){
+              //If current user has budgets notification enabled
+              // Check if the user has budgets and if notifications are enabled
+            $users = User::where('receive_notifications', true)->get();
+              // Log the budget check
             \Log::info('Budget check started');
-              $budgets = Budget::all();
+            
+            foreach($users as $user){
+              $schedule->call(function () use ($user){
+                \Log::info("Budget check started for user ID: {$user->id}");
+                $budgets = Budget::where('user_id', $user->id)->get();
               foreach($budgets as $budget) {
                 $budget->checkSpending();
-              }        
-       })->dailyAt('08:00'); // Adjust the time as needed
+              }
+              })->dailyAt($user->send_notification_time);        
+            }
+              
+       
     }
 
     /**
